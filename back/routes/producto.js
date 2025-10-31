755e8e705837cdafec9ed0ca0199da89b0e77585
gonza laptop <gonz.r@hotmail.com>
2025-10-31 19:57:01 -0300

    Actualizar lógica de creación y recuperación de productos; se corrige nombre y precio, y se implementa recuperación por ID.

--- back/routes/producto.js
+++ back/routes/producto.js
@@ -28,7 +28,37 @@ router.post("/", async(req, res) => {
 });
 
 router.get("/", async(req, res) => {
-    
+    try {
+    const productos = await Producto.findAll();
+
+    return res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Error interno" });
+  }
 });
+
+//traer producto por id
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const producto = await Producto.findByPk(id);
+
+        if (!producto) {
+            return res.status(404).json({ message: "Producto no encontrado" });
+        }
+
+        return res.status(200).json(producto);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Error interno del servidor" });
+    }
+});
+
+//traer productos por categoria
+
+//modificar un producto 
+//dar de baja un producto (baja logica)
 
 module.exports = router;
