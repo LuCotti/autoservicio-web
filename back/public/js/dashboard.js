f1ba3a59a646cba18e979dddbbb6da4536a25545
LuCotti <lucianoarielcotti@gmail.com>
2025-11-08 17:06:36 -0300

    Agregar condicional para que no se muestre la tabla si no hay productos y botón para eliminar de la DB

--- back/public/js/dashboard.js
+++ back/public/js/dashboard.js
@@ -2,6 +2,7 @@ import { serverUrl } from './variables.js';
 const tableBody = document.getElementById('table-body');
 const arrayBtnModificar = tableBody.getElementsByClassName('modificar');
 const arrayBtnBajar = tableBody.getElementsByClassName('bajar');
+const arrayBtnEliminar = tableBody.getElementsByClassName('eliminar');
 
 for (let boton of arrayBtnModificar) {
   document.getElementById(boton.id).onclick = () => window.location.href = '/producto/modificar';
@@ -17,6 +18,25 @@ for (let boton of arrayBtnBajar) {
       });
       if (response.ok) {
         console.log(`Producto Nº ${id} dado de baja correctamente`);
+        location.reload();
+      } else {
+        console.log('Error');
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+}
+
+for (let boton of arrayBtnEliminar) {
+  document.getElementById(boton.id).onclick = async () => {
+    try {
+      const id = boton.id.split("-")[2];
+      const response = await fetch(`${serverUrl}/producto/${id}`, {
+        method: "DELETE"
+      });
+      if (response.ok) {
+        location.reload();
       } else {
         console.log('Error');
       }
