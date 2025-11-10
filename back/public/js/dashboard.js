b7660b939a0f3270bed5147898633ef785097952
LuCotti <lucianoarielcotti@gmail.com>
2025-11-09 23:41:15 -0300

    Mover las variables de botones dentro de un condicional que valide que están en el body

--- back/public/js/dashboard.js
+++ back/public/js/dashboard.js
@@ -2,9 +2,6 @@ import { serverUrl } from './variables.js';
 const btnAgregarProducto = document.getElementById('btn-agregar-producto');
 const btnCerrarSesion = document.getElementById('btn-cerrar-sesion');
 const tableBody = document.getElementById('table-body');
-const arrayBtnModificar = tableBody.getElementsByClassName('modificar');
-const arrayBtnBajar = tableBody.getElementsByClassName('bajar');
-const arrayBtnEliminar = tableBody.getElementsByClassName('eliminar');
 
 btnAgregarProducto.onclick = () => {
   location.assign('/producto/alta');
@@ -14,48 +11,53 @@ btnCerrarSesion.onclick = () => {
   location.replace('/administrator');
 };
 
-for (let boton of arrayBtnModificar) {
-  document.getElementById(boton.id).onclick = () => {
-    const id = boton.id.split('-')[2];
-    localStorage.setItem('id-modificar', id);
-    location.assign('/producto/modificar');
-  };
-}
+if (tableBody) {
+  const arrayBtnModificar = tableBody.getElementsByClassName('modificar');
+  for (let boton of arrayBtnModificar) {
+    document.getElementById(boton.id).onclick = () => {
+      const id = boton.id.split('-')[2];
+      localStorage.setItem('id-modificar', id);
+      location.assign('/producto/modificar');
+    };
+  }
 
-for (let boton of arrayBtnBajar) {
-  document.getElementById(boton.id).onclick = async () => {
-    try {
-      const id = boton.id.split("-")[2];
-      console.log(id);
-      const response = await fetch(`${serverUrl}/producto/activo/${id}`, {
-        method: "PUT"
-      });
-      if (response.ok) {
-        console.log(`Producto Nº ${id} dado de baja correctamente`);
-        location.reload();
-      } else {
-        console.log('Error');
+  const arrayBtnBajar = tableBody.getElementsByClassName('bajar');
+  for (let boton of arrayBtnBajar) {
+    document.getElementById(boton.id).onclick = async () => {
+      try {
+        const id = boton.id.split("-")[2];
+        console.log(id);
+        const response = await fetch(`${serverUrl}/producto/activo/${id}`, {
+          method: "PUT"
+        });
+        if (response.ok) {
+          console.log(`Producto Nº ${id} dado de baja correctamente`);
+          location.reload();
+        } else {
+          console.log('Error');
+        }
+      } catch (error) {
+        console.log('Error:', error);
       }
-    } catch (error) {
-      console.log('Error:', error);
-    }
-  };
-}
+    };
+  }
 
-for (let boton of arrayBtnEliminar) {
-  document.getElementById(boton.id).onclick = async () => {
-    try {
-      const id = boton.id.split("-")[2];
-      const response = await fetch(`${serverUrl}/producto/${id}`, {
-        method: "DELETE"
-      });
-      if (response.ok) {
-        location.reload();
-      } else {
-        console.log('Error');
+  const arrayBtnEliminar = tableBody.getElementsByClassName('eliminar');
+  for (let boton of arrayBtnEliminar) {
+    document.getElementById(boton.id).onclick = async () => {
+      try {
+        const id = boton.id.split("-")[2];
+        const response = await fetch(`${serverUrl}/producto/${id}`, {
+          method: "DELETE"
+        });
+        if (response.ok) {
+          location.reload();
+        } else {
+          console.log('Error');
+        }
+      } catch (error) {
+        console.log('Error:', error);
       }
-    } catch (error) {
-      console.log('Error:', error);
-    }
-  };
+    };
+  }
 }
