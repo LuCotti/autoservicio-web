8e8345a7ab5ba4ba745e88d7bbee592cd5ab9781
LuCotti <lucianoarielcotti@gmail.com>
2025-11-14 19:55:50 -0300

    Aclarar las foreign keys en las relaciones para evitar ambigüedades

--- back/models/relaciones.js
+++ back/models/relaciones.js
@@ -1,7 +1,7 @@
 const Producto = require('./producto');
 const Venta = require('./venta');
 
-Producto.belongsToMany(Venta, { through: 'ProductoVenta' });
-Venta.belongsToMany(Producto, { through: 'ProductoVenta' });
+Producto.belongsToMany(Venta, { through: 'producto_venta', foreignKey: 'productoId' });
+Venta.belongsToMany(Producto, { through: 'producto_venta', foreignKey: 'ventaId' });
 
 module.exports = { Producto, Venta };
