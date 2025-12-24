9889b28cbc7800171872752fe1b5aabc0a650b13
LuCotti <lucianoarielcotti@gmail.com>
2025-12-24 20:02:33 -0300

    .

--- back/db/sequelize.js
+++ back/db/sequelize.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 const stringDb = process.env.STRING_DB;
-const sequelize = new Sequelize(stringDb);
+const sequelize = new Sequelize(stringDb, { dialectModule: require('mysql2') });
 sequelize.sync();
 
 export default sequelize;
