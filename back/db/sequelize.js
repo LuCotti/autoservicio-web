4d93dcf41a7415387f456bb8f25c7a91d46326db
LuCotti <lucianoarielcotti@gmail.com>
2025-12-25 20:08:05 -0300

    Agregar SSL

--- back/db/sequelize.js
+++ back/db/sequelize.js
@@ -2,7 +2,15 @@ import { Sequelize } from 'sequelize';
 import mysql2 from 'mysql2';
 
 const stringDb = process.env.STRING_DB;
-const sequelize = new Sequelize(stringDb, { dialectModule: mysql2 });
-sequelize.sync();
+const sequelize = new Sequelize(stringDb, {
+  dialect: 'mysql',
+  dialectModule: mysql2,
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
+// sequelize.sync();
 
 export default sequelize;
